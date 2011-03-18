48151e349664e82beff7091f6fce2cf1dd9b957d
cerberosnash@gmail.com <cerberosnash@gmail.com@2fb850a2-3156-fb6b-2ae0-c8b3db6d10d8>
2011-03-18 23:35:49 +0000

    [FIXED]  - correcao de conflito no controller de pagamento

--- trunk/app/application/modules/default/controllers/PagamentoController.php
+++ trunk/app/application/modules/default/controllers/PagamentoController.php
@@ -2,6 +2,13 @@
 
 class PagamentoController extends Base_Controller_Action {
 
+    public function init() {
+        parent::init();
+        if ($this->_session->perfil != $this->view->originalController) {
+            //$this->_redirect('/');
+        }
+    }
+
     public function indexAction() {
         $this->_helper->layout->disableLayout();
         if ($this->_session->usuario->tx_perfil != $this->view->originalController) {
