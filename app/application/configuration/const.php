cf17c37c665af10f3c62328e452d5ab9a2591018
CERBEROSNASH <cerberosnash@gmail.com>
2011-03-31 01:04:30 +0000

    [ADD]  - adicionados os arquivos para geracao de relatorios

--- app/application/configuration/const.php
+++ app/application/configuration/const.php
@@ -1,12 +1,13 @@
 <?php
 
+// Constantes do Sistema
 $systemName = str_ireplace('/public/index.php', '', $_SERVER['PHP_SELF']);
 define('SYSTEM_PATH', "http://{$_SERVER['HTTP_HOST']}{$systemName}");
 define('SYSTEM_PATH_ABSOLUTE', "{$_SERVER['DOCUMENT_ROOT']}{$systemName}");
 define('SYSTEM_NAME', str_replace('/', '', $systemName));
 define('BOLETO_PATH', "http://{$_SERVER['HTTP_HOST']}{$systemName}/library/Util/Boletos/");
 
-// Constante Email
+// Constantes PHPMAILER
 define('SYSTEM_EMAIL_USER', "sistema.camisetas.personalizadas@gmail.com");
 define('SYSTEM_EMAIL_PASS', "doctrine");
 define('SYSTEM_EMAIL_ADDRESS_FROM', "sistema.camisetas.personalizadas@gmail.com");
@@ -15,3 +16,13 @@ define('SYSTEM_SMTP_HOST', "ssl://smtp.gmail.com");
 define('SYSTEM_SMTP_PORT', 465);
 define('SYSTEM_SMTP_DEBUGGER', false);
 define('SYSTEM_EMAIL_TEMPLATES', SYSTEM_PATH_ABSOLUTE . '/public/templates/email/');
+
+//Constantes PHPJasperXML
+define('PHPJasperXML_HOST', "localhost");
+define('PHPJasperXML_DB', "camisetas");
+define('PHPJasperXML_USER', "postgres");
+define('PHPJasperXML_PASS', "postgres");
+define('PHPJasperXML_DRIVER', "psql");
+define('PHPJasperXML_PORT', 5432);
+define('PHPJasperXML_VERSION', "0.7c");
+define('PHPJasperXML_LIBPDF', "fpdf");
